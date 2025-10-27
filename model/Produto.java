84c87a56b99504a435982e59d50c5882c915c7d4
kd14c <carlos.campolongo@fatec.sp.gov.br>
2025-10-27 20:10:02 -0300

    Create Produto.java

---
+++ model/Produto.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.Objects;
+
+public class Produto {
+    private String id;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto() {}
+
+    public Produto(String id, String nome, double preco, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+    public int getQuantidade() { return quantidade; }
+    public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %s | Nome: %s | Preço: %.2f | Quantidade: %d",
+                id, nome, preco, quantidade);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
